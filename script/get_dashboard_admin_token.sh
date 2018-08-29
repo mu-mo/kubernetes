87419ee033d17dd105b1ba83cd48fdc9a42162c8
mu-mo <2205567926@qq.com>
2018-08-29 21:17:56 +0800

    add README.md

---
+++ script/get_dashboard_admin_token.sh
@@ -0,0 +1,1 @@
+kubectl -n kube-system describe secret/$(kubectl -n kube-system get secret | grep kubernetes-dashboard-admin | awk '{print $1}') | grep token
---
+++ script/pull_image.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+images=(
+    kube-apiserver-amd64:v1.11.2
+    kube-controller-manager-amd64:v1.11.2
+    kube-scheduler-amd64:v1.11.2
+    kube-proxy-amd64:v1.11.2
+    pause:3.1
+    etcd-amd64:3.2.18
+    coredns:1.1.3
+
+    pause-amd64:3.1
+
+    kubernetes-dashboard-amd64:v1.10.0
+    heapster-amd64:v1.5.4
+    heapster-grafana-amd64:v5.0.4
+    heapster-influxdb-amd64:v1.5.2
+)
+
+for imageName in ${images[@]} ; do
+    docker pull registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName
+    docker tag registry.cn-hangzhou.aliyuncs.com/google_containers/$imageName k8s.gcr.io/$imageName
+done
+
+
+# calico
+calico_images=(
+    quay.io/calico/node:v3.1.3
+    quay.io/calico/cni:v3.1.3
+    quay.io/calico/typha:v0.7.4
+)
+
+for imageName in ${calico_images[@]} ; do
+    docker pull $imageName
+done
